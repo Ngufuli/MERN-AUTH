2bff68b6a47668a182cd4a68673efff5fc0d7bd9
Ngufuli <ngufuli@outlook.com>
2023-05-21 04:11:13 +0530

    node environment and port

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
 
-const PORT = 5000;
+dotenv.config()
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res)=>res.send('Server is ready'));
 
-app.liste(PORT, ()=>console.log(`Server has started at port${PORT}`));
+app.listen(PORT, ()=>console.log(`Server has started at port: ${PORT}`));
 
