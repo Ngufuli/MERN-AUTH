2c66e1da50d8178e8c251ec9875164e278204e15
Ngufuli <ngufuli@outlook.com>
2023-05-21 15:10:12 +0530

    middleware, error handler

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
+import { notFound, erroHandler } from './middleware/errorMiddleware.js';
+dotenv.config()
 
 const app = express();
+app.use('/api/users', userRoutes);
 
-dotenv.config()
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res)=>res.send('Server is ready'));
+app.use(notFound);
+app.use(erroHandler);
 
 app.listen(PORT, ()=>console.log(`Server has started at port: ${PORT}`));
 
