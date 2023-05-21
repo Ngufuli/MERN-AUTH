5ffbc745cab75ae81cfb70e47002a51fc971425f
Ngufuli <ngufuli@outlook.com>
2023-05-21 16:06:18 +0530

    .

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userControllers.js';
+import { authUser,
+        registerUser,
+        logoutUser,
+        getUserProfile,
+        updateUserProfile } from '../controllers/userControllers.js';
 
+router.post('/', registerUser);
 router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router.route('/profile').put(updateUserProfile).get(getUserProfile);
 
 export default router;
