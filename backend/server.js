94b879db0c3ffecf30c47d957756a92ed950846c
Ngufuli <ngufuli@outlook.com>
2023-05-22 13:04:42 +0530

    middleware to interact with requests

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ dotenv.config()
 
 connectDB();
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
