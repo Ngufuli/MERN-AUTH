1e47390ab1786b3be7c620ce3dd4b9c850e42825
Ngufuli <ngufuli@outlook.com>
2023-05-28 15:28:45 +0530

    end cookies sesion in logout

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -67,7 +67,7 @@ const logoutUser = asyncHandler( async (req, res)=>{
         httpOnly: true,
         expires: new Date(0)
     })
-    res.status(200).json({message: 'Logout User'});
+    res.status(200).json({message: 'User logged out'});
 })
 
 
