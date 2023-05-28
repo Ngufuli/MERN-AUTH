d851bc81014db3be9ef064e172885cca0aefb0b6
Ngufuli <ngufuli@outlook.com>
2023-05-28 15:28:34 +0530

    end cookies sesion in logout

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -63,6 +63,10 @@ const registerUser = asyncHandler( async (req, res)=>{
 //@routes   POST api/users/logout
 //@access   Public
 const logoutUser = asyncHandler( async (req, res)=>{
+    res.cookie('jwt','',{
+        httpOnly: true,
+        expires: new Date(0)
+    })
     res.status(200).json({message: 'Logout User'});
 })
 
