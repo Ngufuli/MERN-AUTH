d678d94629c8af0537f463d888b4058df6a884a9
Ngufuli <ngufuli@outlook.com>
2023-05-29 14:50:50 +0530

    minor changes on login and working on getProfile controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,10 +5,11 @@ import { authUser,
         logoutUser,
         getUserProfile,
         updateUserProfile } from '../controllers/userControllers.js';
+import {protect} from '../middleware/authMiddleware.js';
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile').put(updateUserProfile).get(getUserProfile);
+router.route('/profile').put(protect, updateUserProfile).get(protect, getUserProfile);
 
 export default router;
