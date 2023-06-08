1d46a708ec907ff0b1b15afecfd19e12cd2b448f
Ngufuli <ngufuli@outlook.com>
2023-06-08 15:40:48 +0530

    header updates

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,9 +2,28 @@ import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 import {useSelector, useDispatch} from 'react-redux';
 import {LinkContainer} from 'react-router-bootstrap';
+import {useNavigate} from 'react-router-dom';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import {logout} from '../slices/authSlice';
 
 const Header = () => {
   const {userInfo} = useSelector((state) => state.auth)
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLogoutMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <header>
@@ -26,7 +45,7 @@ const Header = () => {
                   Profile
                 </NavDropdown.Item>
               </LinkContainer>
-              <NavDropdown.Item>
+              <NavDropdown.Item onClick={logoutHandler}>
                   Logout
               </NavDropdown.Item>
             </NavDropdown>
