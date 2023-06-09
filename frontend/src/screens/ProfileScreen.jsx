2af2a68dbb2de3d4a2c64b7c3735e2975d532e8a
Ngufuli <ngufuli@outlook.com>
2023-06-09 15:10:52 +0530

    profile update

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import FormContainer from '../components/FormContainer';
 import Loader from '../components/Loader';
+import { useUpdateUseMutation } from '../slices/usersApiSlice';
 // import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -20,6 +21,8 @@ const RegisterScreen = () => {
 
   const { userInfo } = useSelector((state) => state.auth);
 
+  const [updateProfile, {isLoading}] = useUpdateUseMutation();
+
   useEffect(()=>{
     setName(userInfo.name);
     setEmail(userInfo.email)
@@ -31,7 +34,18 @@ const RegisterScreen = () => {
     if (password !== confirmPassword) {
       toast.error('Passwords do not match');
     } else {
-      console.log('submit')
+      try {
+        const res = await updateProfile({
+            _id: userInfo._id,
+            name,
+            email,
+            password
+        }).unwrap();
+        dispatch(setCredentials({...res}));
+        toast.success("Profile Updated");
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
     }
   };
   return (
@@ -76,6 +90,8 @@ const RegisterScreen = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
+
+        {isLoading && <Loader/>}
 
         <Button type='submit' variant='primary' className='mt-3'>
           Update
